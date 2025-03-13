33174e90e2b8513e598987da869df50c88f2010e
Leyder Quintero <desarrollador9@wedevel.co>
2025-03-13 03:22:01 -0500

    change name to createPublicationLike

--- apps/publications/src/publication-likes/infraestructure/controllers/PublicationLikeController.ts
+++ apps/publications/src/publication-likes/infraestructure/controllers/PublicationLikeController.ts
@@ -2,13 +2,13 @@ import { Request, Response } from "express";
 import { CreatePublicationLike } from "../../application/createPublicationLike";
 
 export class PublicationLikeController {
-  constructor(private createPublication: CreatePublicationLike) {}
+  constructor(private createPublicationLike: CreatePublicationLike) {}
 
   async run(req: Request, res: Response): Promise<void> {
     try {
       const {publicationId, userId} = req.body;
 
-      const publication = await this.createPublication.run(publicationId, userId);
+      const publication = await this.createPublicationLike.run(publicationId, userId);
 
       res.status(200).json({ message: "Like success", publication });
     } catch (error) {
